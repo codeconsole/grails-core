55a45f5871e5b1ce8980aa638972e452aae9e108
Scott Murphy Heiberg <scott@alwaysvip.com>
2026-05-16 18:29:06 -0700

    fix checkstyle ImportOrder: separate spring.beans import group

--- grails-core/src/main/groovy/grails/util/GrailsUtil.java
+++ grails-core/src/main/groovy/grails/util/GrailsUtil.java
@@ -22,6 +22,7 @@ import java.io.PrintWriter;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.springframework.beans.BeanUtils;
 
 import grails.config.Config;
