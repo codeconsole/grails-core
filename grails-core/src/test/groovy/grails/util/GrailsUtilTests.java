8c6476c17e39ff85a3c4057dbc928049e45f2a9d
jamesfredley <jamesfredley@users.noreply.github.com>
2026-08-02 22:13:31 +0000

    [skip ci] Release v7.0.15

--- grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
+++ grails-core/src/test/groovy/grails/util/GrailsUtilTests.java
@@ -33,7 +33,7 @@ public class GrailsUtilTests {
 
     @Test
     public void testGrailsVersion() {
-        assertEquals("7.0.15-SNAPSHOT", GrailsUtil.getGrailsVersion());
+        assertEquals("7.0.15", GrailsUtil.getGrailsVersion());
     }
 
     @AfterEach
