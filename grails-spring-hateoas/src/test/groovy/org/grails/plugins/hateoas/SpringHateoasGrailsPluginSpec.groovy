a15a0be5d3f16d714a34efd00c0fe0fecc11cd87
Scott Murphy Heiberg <scott@alwaysvip.com>
2026-08-28 14:57:51 -0700

    feat: add optional Spring HATEOAS integration

---
+++ grails-spring-hateoas/src/test/groovy/org/grails/plugins/hateoas/SpringHateoasGrailsPluginSpec.groovy
@@ -0,0 +1,42 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.grails.plugins.hateoas
+
+import org.springframework.beans.factory.support.BeanRegistryAdapter
+import org.springframework.beans.factory.support.DefaultListableBeanFactory
+import org.springframework.core.env.StandardEnvironment
+
+import grails.rest.render.hateoas.GrailsSpringHateoasAdapter
+
+import spock.lang.Specification
+
+class SpringHateoasGrailsPluginSpec extends Specification {
+
+    void 'registers the optional Spring HATEOAS adapter'() {
+        given:
+        def beanFactory = new DefaultListableBeanFactory()
+        def registrar = new SpringHateoasGrailsPlugin().beanRegistrar()
+
+        when:
+        new BeanRegistryAdapter(beanFactory, new StandardEnvironment(), registrar.class).register(registrar)
+
+        then:
+        beanFactory.getBean('grailsSpringHateoasAdapter', GrailsSpringHateoasAdapter)
+    }
+}
